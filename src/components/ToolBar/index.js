2320aba56c74fa0b232e77399d546c64c7bb0d46
Shahreyar <mdshahreyarhannan@gmail.com>
2026-04-29 10:25:41 +0530

    feat:add tool bar with icons

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -1,13 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import classes from "./index.module.css";
 // import styles from "./index.module.css"
 
-function ToolBar(){
+import { LuRectangleHorizontal } from "react-icons/lu";
+import { FaSlash, FaRegCircle, FaEraser, FaArrowRight, FaPaintBrush, FaDownload, FaFont, FaUndo, FaRedo } from "react-icons/fa";
+
+function ToolBar() {
+    const [activeTool, setActiveTool] = useState("LINE");
     return (
         <div className={classes.container}>
-            <div className={classes.toolItem}>A</div>
-            <div className={classes.toolItem}>B</div>
+            <div className={`${classes.toolItem} ${activeTool === "LINE" ? classes.active : ""}`} onClick={() => setActiveTool("LINE")}><FaSlash /></div>
+            <div className={`${classes.toolItem} ${activeTool === "RECTANGLE" ? classes.active : ""}`} onClick={() => setActiveTool("RECTANGLE")}><LuRectangleHorizontal /></div>
         </div>
     )
 }
