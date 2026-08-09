542b559a990931d8d7847b61d40b71848ee775ad
Shahreyar <mdshahreyarhannan@gmail.com>
2026-08-09 18:05:40 +0530

    dashboard UI complete

--- src/components/Dashboard/Canvas.jsx
+++ src/components/Dashboard/Canvas.jsx
@@ -2,14 +2,37 @@
 import React from "react";
 import classes from "./index.module.css"
 
-function Canvas({canvas}){
+function Canvas({ canvas }) {
 
-    return(
-        <div className={classes.canvasContainer}>
-            <div className={classes.canvasCard}>
-                <h4>{canvas.owner.name}</h4>
+    const findEditDuration = () => {
+        let seconds = (new Date() - new Date(canvas.modifiedAt))/1000;
+        const day = Math.floor(seconds/(3600*24));
+        seconds = seconds%(3600*24);
+        const hour = Math.floor(seconds/3600);
+        seconds = seconds%3600;
+        const minute = Math.floor(seconds/60);
+        seconds = seconds%60;
+        if(day>0){
+            return `${day}d`;
+        }else if(hour>0){
+            return `${hour}h`
+        }else if(minute>0){
+            return `${minute}m`
+        }
+
+        return `${seconds}s`
+    }
+    return (
+        <div className={classes.canvasCard}>
+            <div className={classes.topPart}>
+
+            </div>
+            <div className={classes.bottomPart}>
+                <h5 className={classes.canvasName}>Canvas</h5>
+                <p>Edited <span className={classes.editDuration}>{findEditDuration()} </span>ago</p>
             </div>
         </div>
+
     )
 }
 
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -28,22 +28,56 @@ function Dashboard(){
         fetchCanvas();
     },[])
 
+    const capitalize = (str) => str? str.charAt(0).toUpperCase() + str.slice(1):str; 
+
 
     return (
-       loader? <p className={classes.dashLoader}>{loader}</p>: <div className={classes.dashBoardBackground}>
+       loader? <p className={classes.dashLoader}>{loader}</p>: 
+       <div className={classes.dashBoardBackground}>
             <div className={classes.dashBoardContainer}>
 
                 <div className={classes.navBar}>
-                    <h3>Welcome {userData.name}</h3>
+                    <h2 className={classes.logo}>Whiteboard</h2>
+                    <button className={classes.profilePicture}>{userData.name?userData.name.slice(0,2).toUpperCase():'WB'}</button>
                 </div>
 
+
                 <div className={classes.contents}>
+                <div className={classes.welcomeSection}>
+                    <div className={classes.welcomeMessage}>
+                        <h3>Welcome Back, {userData? capitalize(userData.name): "Creator"}</h3>
+                        <h4>Your creative workspace</h4>
+                    </div>
+
+                    <div className={classes.profileCard}>
+                        <div className={classes.profileInfo}>
+                            <div className={classes.profileAvatar}>
+                                {userData.name?userData.name.slice(0,2).toUpperCase():'WB'}
+                            </div>
+                            <div className={classes.userInfo}>
+                                <h4 className={classes.name}>{userData? capitalize(userData.name): "Creator"}</h4>
+                                <h4 className={classes.email}>{userData? userData.email: "creator@gmail.com"}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={classes.canvasSection}>
+                    <div className={classes.header}>
+                        <h4>Your Canvases</h4>
+                        <button>+ Create Canvas</button>
+                    </div>
+
+                    <div className={classes.canvasGrid}>
+
                     {canvases.length===0?<p className={classes.noCanvas}>No Canvas Found!</p>:canvases.map((canvas)=>{
                         return (<Canvas key={canvas._id} canvas={canvas} />)
                     })}
+                    </div>
+                </div>
                 </div>
                 <div className={classes.dashFooter}>
-                    <button type="submit">Create</button>
+                    <button type="submit" className={classes.createBtn}>Create</button>
                 </div>
             </div>
         </div>
